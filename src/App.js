1fd4c4d97b45686c889af4d7605644dbe4b3bfac
steve kaci <stevekaci01@gmail.com>
2021-11-08 16:22:41 +0000

    Ajout un beau header bootstrap

--- src/App.js
+++ src/App.js
@@ -7,21 +7,35 @@ function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" width="50"/>
-      
-        <h2>Header</h2>
-        <div class="btn-group" role="group" aria-label="...">
-  <button type="button" class="btn btn-default">Left</button>
-  <button type="button" class="btn btn-default">Middle</button>
-  <button type="button" class="btn btn-default">Right</button>
-</div>
-<section>
-  <h1>Contenu</h1>
-</section>
+        <nav class="navbar navbar-expand-lg navbar-dark bg-dark">
+          <div class="container-fluid">
+            <a class="navbar-brand" href="#"><img src={logo} className="App-logo" alt="logo" width="50" /></a>
+            <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
+              <span class="navbar-toggler-icon"></span>
+            </button>
+            <div class="collapse navbar-collapse" id="navbarNav">
+              <ul class="navbar-nav">
+                <li class="nav-item">
+                  <a class="nav-link active" aria-current="page" href="#">Home</a>
+                </li>
+                <li class="nav-item">
+                  <a class="nav-link" href="#">Gallery</a>
+                </li>
+                <li class="nav-item">
+                  <a class="nav-link" href="#">Contact</a>
+                </li>
+          
+              </ul>
+            </div>
+          </div>
+        </nav>
+        <section>
+          <h1>Contenu</h1>
+        </section>
 
-<footer>
-  <h2>Footer</h2>
-</footer>
+        <footer className="bg-dark text-white text-center p-2">
+          <h2>© Mon site de ouf</h2>
+        </footer>
 
 
       </header>
