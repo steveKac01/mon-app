4aa9a0fd1faa0fdb393480aff94528be5438cb6f
steve kaci <stevekaci01@gmail.com>
2021-11-08 15:12:58 +0000

    Ajout header

--- src/App.js
+++ src/App.js
@@ -5,18 +5,18 @@ function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        <img src={logo} className="App-logo" alt="logo" width="50"/>
+        
+        <h2>Header</h2>
+<section>
+  <h1>Contenu</h1>
+</section>
+
+<footer>
+  <h2>Footer</h2>
+</footer>
+
+
       </header>
     </div>
   );
