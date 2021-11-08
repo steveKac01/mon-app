5495f0e84de0dc98e0304b689578eb5e3964171e
steve kaci <stevekaci01@gmail.com>
2021-11-08 16:05:16 +0000

    Added bootstrap library

--- src/App.js
+++ src/App.js
@@ -1,13 +1,20 @@
 import logo from './logo.svg';
 import './App.css';
+import 'bootstrap/dist/css/bootstrap.css';
+import 'bootstrap/dist/js/bootstrap.bundle';
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" width="50"/>
-        
+      
         <h2>Header</h2>
+        <div class="btn-group" role="group" aria-label="...">
+  <button type="button" class="btn btn-default">Left</button>
+  <button type="button" class="btn btn-default">Middle</button>
+  <button type="button" class="btn btn-default">Right</button>
+</div>
 <section>
   <h1>Contenu</h1>
 </section>
