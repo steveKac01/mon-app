df6ffab1e5e3ca0fe796e6fbdd3b783ed17e8b86
steveKac01 <stevekaci01@gmail.com>
2021-11-10 16:03:55 +0000

    Finished, for real this time, i m sure now, promised

--- src/Components/Header.js
+++ src/Components/Header.js
@@ -1,4 +1,5 @@
 import logo from '../Img/logo.svg';
+import {NavLink} from 'react-router-dom';
 
 
 const Header = () => {
@@ -6,22 +7,22 @@ const Header = () => {
         <header className="App-header">
             <nav class="navbar navbar-expand-lg navbar-dark bg-dark">
                 <div class="container-fluid">
-                    <a class="navbar-brand" href="/"><img src={logo} className="App-logo" alt="logo" width="50" /></a>
+                    <NavLink class="navbar-brand" to="/"><img src={logo} className="App-logo" alt="logo" width="50" /></NavLink>
                     <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
                         <span class="navbar-toggler-icon"></span>
                     </button>
                     <div class="collapse navbar-collapse" id="navbarNav">
                         <ul class="navbar-nav">
                             <li class="nav-item">
-                                <a class="nav-link active" aria-current="page" href="/">Home</a>
+                                <NavLink className="nav-link active" aria-current="page" to="/">Home</NavLink>
                             </li><li class="nav-item">
-                                <a class="nav-link" href="/article">Article</a>
+                                <NavLink className="nav-link" to="/article">Article</NavLink>
                             </li>
                             <li class="nav-item">
-                                <a class="nav-link" href="/gallery">Gallery</a>
+                                <NavLink className="nav-link" to="/gallery">Gallery</NavLink>
                             </li>
                             <li class="nav-item">
-                                <a class="nav-link" href="/contact">Contact</a>
+                                <NavLink className="nav-link" to="/contact">Contact</NavLink>
                             </li>
 
                         </ul>
