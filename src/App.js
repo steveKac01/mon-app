d600e87f3160a33ad54590f4fcc8ee1b795f5fa1
steveKac01 <stevekaci01@gmail.com>
2021-11-10 11:35:18 +0000

    Duppliquer

--- src/App.js
+++ src/App.js
@@ -5,14 +5,16 @@ import 'bootstrap/dist/js/bootstrap.bundle';
 import Footer from './Components/Footer';
 import Header from './Components/Header';
 import Home from './Pages/Home';
+import Gallery from './Pages/Gallery';
+import Contact from './Pages/Contact';
 
 function App() {
   return (
     <div className="App">
       <Header />
-
       <Home />
-
+      <Gallery />
+      <Contact />
       <Footer />
     </div>
   );
