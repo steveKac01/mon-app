268a83bf384a8786887eafe78fbe44503c241848
Steve kaci <Steve.kaci@k-net.fr>
2021-11-18 11:16:11 +0000

    build 1.0

--- src/Components/Header.js
+++ src/Components/Header.js
@@ -14,7 +14,7 @@ const Header = () => {
                     <div className="collapse navbar-collapse" id="navbarNav">
                         <ul className="navbar-nav">
                             <li className="nav-item">
-                                <NavLink className="nav-link active" aria-current="page" to="/">Home</NavLink>
+                                <NavLink className="nav-link" aria-current="page" to="/">Home</NavLink>
                             </li><li className="nav-item">
                                 <NavLink className="nav-link" to="/article">Article</NavLink>
                             </li>
