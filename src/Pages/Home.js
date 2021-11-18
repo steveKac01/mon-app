4dbcb41652335c04b310094f0ee271c3086890f1
Steve kaci <Steve.kaci@k-net.fr>
2021-11-18 11:59:52 +0000

    modif test

--- src/Pages/Home.js
+++ src/Pages/Home.js
@@ -12,7 +12,7 @@ const Home = () => {
              <div className="col-12 col-md-6">
               <p>Lorem Ipsum is simply <b>dummy</b> text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum.</p>
               <div>
-                <h1>TRAILER</h1>
+                <h1>-- TRAILER --</h1>
                 <iframe width="560" height="315" src="https://www.youtube.com/embed/e5wwSxl0atc" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe></div>
            
               </div>
